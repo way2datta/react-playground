120399eff243d7a0423e0772f5936bf66dc1e65e
Dagttatray Kale <way2datta@gmail.com>
2019-09-27 21:55:36 +0530

    Customers listing is using material ui table

--- movie-rental-simple/src/CustomersListing.js
+++ movie-rental-simple/src/CustomersListing.js
@@ -1,5 +1,27 @@
 import React from "react";
 import axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    width: '100%',
+    overflowX: 'auto',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
 
 export class CustomersListing extends React.Component {
   state = {
@@ -23,30 +45,34 @@ export class CustomersListing extends React.Component {
 }
 
 function Gridview({ datasource }) {
+  const classes = useStyles();
+
   if (!datasource.length) {
     return <h2>No record found.</h2>
   }
 
   return <>
-    <table>
-      <thead>
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
+    <Paper className={classes.paper}>
+      <Table className={classes.table} size="small">
+        <TableHead>
+          <TableRow>
+          <TableCell>First name</TableCell>
+          <TableCell>Last name</TableCell>
+          <TableCell>Email</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
         {
           datasource.map(x => {
             return <tr key={x.id}>
-              <td>{x.firstName}</td>
-              <td>{x.lastName}</td>
-              <td>{x.email}</td>
+              <TableCell>{x.firstName}</TableCell>
+              <TableCell>{x.lastName}</TableCell>
+              <TableCell>{x.email}</TableCell>
             </tr>
           })
         }
-      </tbody>
-    </table>
+        </TableBody>
+      </Table>
+    </Paper>
   </>
 }
