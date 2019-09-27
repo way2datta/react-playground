ca97f5049fcd9c5562788bc1780e99e1a1b6e52d
Dagttatray Kale <way2datta@gmail.com>
2019-09-27 15:25:18 +0530

    Router

--- movie-rental-simple/src/App.js
+++ movie-rental-simple/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route path="/customers" component={CustomerListing} />
+          <Route path="/books" component={Books} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function CustomerListing() {
+  return <h2>About</h2>;
+}
+
+function Books({ match }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Books</h1>
+    );
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/customers">Customers</Link>
+      </li>
+      <li>
+        <Link to="/books">Books</Link>
+      </li>
+    </ul>
   );
 }
 
-export default App;
