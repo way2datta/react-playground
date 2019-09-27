35fcc9ba6ebaaa6136b51447bea5d7dfcdc0efc6
Dagttatray Kale <way2datta@gmail.com>
2019-09-27 15:49:46 +0530

    Books listing

--- movie-rental-simple/src/BooksListing.js
+++ movie-rental-simple/src/BooksListing.js
@@ -1,6 +1,52 @@
 import React from "react";
+import axios from "axios";
+
 export class BooksListing extends React.Component {
-  render() {
-    return (<h1>Books</h1>);
+  state = {
+    datasource: []
   }
+
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:5000/books");
+    this.setState({
+      datasource: response.data
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Books</h1>
+        <Gridview datasource={this.state.datasource} />
+      </>);
+  }
+}
+
+function Gridview({ datasource }) {
+  if (!datasource.length) {
+    return <h2>No record found.</h2>
+  }
+
+  return <>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Author</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          datasource.map(x => {
+            return <tr key={x.id}>
+              <td>{x.name}</td>
+              <td>{x.authorName}</td>
+              <td>{x.status}</td>
+            </tr>
+          })
+        }
+      </tbody>
+    </table>
+  </>
 }
