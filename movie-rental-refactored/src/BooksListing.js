6c54966d682f70e7a0acc742bfd96cfaf958c120
Dagttatray Kale <way2datta@gmail.com>
2019-09-28 18:59:17 +0530

    Gridview added

--- movie-rental-refactored/src/BooksListing.js
+++ movie-rental-refactored/src/BooksListing.js
@@ -1,28 +1,7 @@
 import React from "react";
 import axios from "axios";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import { Gridview } from "./Gridview";
 import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%"
-  },
-  paper: {
-    marginTop: theme.spacing(3),
-    width: "100%",
-    overflowX: "auto",
-    marginBottom: theme.spacing(2)
-  },
-  table: {
-    minWidth: 650
-  }
-}));
 
 export class BooksListing extends React.Component {
   state = {
@@ -42,7 +21,7 @@ export class BooksListing extends React.Component {
   };
 
   handleBorrowBook = (event, id) => {
-    this.props.history.push("/books/"+id+"/borrow");
+    this.props.history.push("/books/" + id + "/borrow");
   };
 
   async fetchAllBooks() {
@@ -53,6 +32,28 @@ export class BooksListing extends React.Component {
   }
 
   render() {
+    const actions = [
+      model => {
+        return model.status === "available" ? (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={e => this.handleBorrowBook(e, model.id)}
+          >
+            Borrow
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={e => this.handleReturnBook(e, model.id)}
+          >
+            Return
+          </Button>
+        );
+      }
+    ];
+
     return (
       <>
         <h1>Books</h1>
@@ -60,67 +61,11 @@ export class BooksListing extends React.Component {
           datasource={this.state.datasource}
           handleReturnBook={this.handleReturnBook}
           handleBorrowBook={this.handleBorrowBook}
+          headers={["Name", "Author", "Status", "Customer", "Action"]}
+          properties={["name", "authorName", "status", "customer"]}
+          actions={actions}
         />
       </>
     );
   }
-}
-
-function Gridview({ datasource, handleReturnBook, handleBorrowBook }) {
-  const classes = useStyles();
-
-  if (!datasource.length) {
-    return <h2>No record found.</h2>;
-  }
-
-  return (
-    <>
-      <Paper className={classes.paper}>
-        <Table className={classes.table} size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Author</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Customer</TableCell>
-              <TableCell>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {datasource.map(x => {
-              return (
-                <TableRow key={x.id}>
-                  <TableCell>{x.name}</TableCell>
-                  <TableCell>{x.authorName}</TableCell>
-                  <TableCell className="text-capitalize">{x.status}</TableCell>
-                  <TableCell>{x.customer || "--"}</TableCell>
-                  <TableCell>
-                    {x.status === "available" ? (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}
-                        onClick={e => handleBorrowBook(e, x.id)}
-                      >
-                        Borrow
-                      </Button>
-                    ) : (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}
-                        onClick={e => handleReturnBook(e, x.id)}
-                      >
-                        Return
-                      </Button>
-                    )}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </Paper>
-    </>
-  );
 }
